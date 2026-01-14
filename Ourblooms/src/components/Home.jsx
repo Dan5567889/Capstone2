c89d45877e6c444e2aa812e1d9e064ea4395b8c3
Dan5567889 <dantehernandz587@gmail.com>
2026-01-14 09:23:06 -0500

    boom

--- Ourblooms/src/components/Home.jsx
+++ Ourblooms/src/components/Home.jsx
@@ -1,5 +1,5 @@
 import Hero from './components/Hero';
-import Footer from './components/Footer.jsx';
+import Footer from './Footer.jsx';
 
 import c from '../assets/carousel_images/Image.png'
 import c1 from '../assets/carousel_images/Image 1.png'
